bdb1fcf1c645e12e5bb255c887b5216498318bb3
joncolque <joncolque@gmail.com>
2019-10-10 17:11:22 -0300

    Se aniade la logica del juego

---
+++ lib/juego.rb
@@ -0,0 +1,42 @@
+class Juego
+	def initialize
+		@partidaActual = 0
+		@paises = ["argentina", "peru", "uruguay", "chile", "brasil"]
+		@capitales = ["buenos aires", "lima", "montevideo", "santiago", "brasilia"]
+		@estado = "jugando"
+	end
+
+	def paises
+		return @paises
+	end
+
+	def partidaActual
+		return @partidaActual
+	end
+
+	def capitales
+		return @capitales
+	end
+
+	def estado
+		return @estado
+	end
+
+	def siguientePartida
+		@partidaActual += 1
+		if (@partidaActual == @paises.length)
+			@estado = "finalizo"
+		end
+	end
+
+	def compararPartida indice, palabra
+			return paises[indice] == palabra
+	end
+
+	def reiniciar
+		@partidaActual = 0
+		@estado = "jugando"
+	end
+	
+
+end
